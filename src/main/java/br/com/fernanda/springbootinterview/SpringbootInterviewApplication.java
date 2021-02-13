740846635c98725010493ef2d081668041642af8
Fernanda Belchior <fernandagfb@outlook.com>
2021-02-13 10:58:39 -0300

    Initial settings

---
+++ src/main/java/br/com/fernanda/springbootinterview/SpringbootInterviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.springbootinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fernanda/springbootinterview/SpringbootInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.springbootinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
